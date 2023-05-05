3fae969ba34bb5b2743d1828781375589d475860
Oscar Santos <oscar303data@gmail.com>
2023-05-05 11:57:51 -0600

    create artist_song index in the routes

--- spec/features/artists/songs/index_songs.rb
+++ spec/features/artists/songs/index_songs.rb
@@ -1,4 +1,22 @@
-As a user
-when i visit an artists songs index
-    then i see the titles all of that artists songs
-    and i see that each title is a link to the songs show page 
+# As a user
+# when i visit an artists songs index
+#     then i see the titles all of that artists songs
+#     and i see that each title is a link to the songs show page 
+
+require 'rails_helper'
+
+RSpec.describe 'Artists songs index page' do
+    it 'shows all the titles of artists songs' do
+        artist = Artist.create!(name: 'The Beatles')
+        song = artist.songs.create!(title: 'And i love her', length: 27348, play_count: 29544)
+        song_2 = artist.songs.create!(title: 'Yesterday', length: 193866, play_count: 99922)
+
+        visit "/artists/#{artist.id}/songs"
+
+        expect(page).to have_content(song.title)
+        expect(page).to have_content(song_2.title)
+
+    end
+
+    it 'links to each songs show page'
+end
