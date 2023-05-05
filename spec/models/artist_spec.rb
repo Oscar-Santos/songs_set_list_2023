1e0e85a348dd5e7d248fff51b605122407f38597
Oscar Santos <oscar303data@gmail.com>
2023-05-05 13:57:49 -0600

    create spec test for artist model

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
   it {should have_many :songs}
+
+  describe 'instance methods' do
+    describe '#average_song_length' do
+      it 'returns the average song length' do
+        artist = Artist.create!(name: 'The Beatles')
+        song = artist.songs.create!(title: 'And i love her', length: 27348, play_count: 29544)
+        song_2 = artist.songs.create!(title: 'Yesterday', length: 193866, play_count: 99922)
+
+        expect(artist.average_song_length).to eq(110607)
+      end
+    end
+
+  end
 end
