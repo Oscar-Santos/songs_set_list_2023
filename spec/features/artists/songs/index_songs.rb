94c48637e0db70e9df30a5f41ca9b3aaa398389c
Oscar Santos <oscar303data@gmail.com>
2023-05-05 14:36:56 -0600

    create average song play count test

--- spec/features/artists/songs/index_songs.rb
+++ spec/features/artists/songs/index_songs.rb
@@ -31,7 +31,7 @@ RSpec.describe 'Artists songs index page' do
 
     end
 
-    it 'shows the average song length fir the artist' do
+    it 'shows the average song length for the artist' do
         artist = Artist.create!(name: 'The Beatles')
         song = artist.songs.create!(title: 'And i love her', length: 27348, play_count: 29544)
         song_2 = artist.songs.create!(title: 'Yesterday', length: 193866, play_count: 99922)
@@ -39,5 +39,15 @@ RSpec.describe 'Artists songs index page' do
         visit "/artists/#{artist.id}/songs"
 
         expect(page).to have_content('Average Song Length for The Beatles: 110607')
+    end
+
+    it 'shows the average song play count for the artist' do
+        artist = Artist.create!(name: 'The Beatles')
+        song = artist.songs.create!(title: 'And i love her', length: 27348, play_count: 29544)
+        song_2 = artist.songs.create!(title: 'Yesterday', length: 193866, play_count: 99922)
+
+        visit "/artists/#{artist.id}/songs"
+
+        expect(page).to have_content('Average Song Play Count for The Beatles: 64733')
     end
 end
