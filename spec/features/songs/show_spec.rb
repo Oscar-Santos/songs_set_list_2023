8b05ec79e399c83fece918d8ffd50929d35ccb0b
Oscar Santos <oscar303data@gmail.com>
2023-05-05 09:19:19 -0600

    add user story in my show spec file

--- spec/features/songs/show_spec.rb
+++ spec/features/songs/show_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
+# as a visitor,
+# when i visit /songs/1
+#     then i see that songs title  and artist
+#     and i dont see any of the other songs in my database
 
 RSpec.describe 'songs show page' do
     it 'displays the song title' do
         song = Song.create(title: 'Cada dia', length: 27348, play_count: 29544)
         
         visit "/songs/#{song.id}"
+
     end
 
     it 'displays the name of the artist for the song'
