70781248b351bbc15fe821f78042cf18ad29f32e
Oscar Santos <oscar303data@gmail.com>
2023-05-08 18:02:32 -0600

    verb delete and action destroy is created for artist in the routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
   get '/artists/:id/edit', to: 'artists#edit'
 
   patch '/artists/:id', to: 'artists#update'
+
+  delete '/artists/:id', to: 'artists#destroy'
 end
